86d37cfd377860f80a32408b46757f6a1a67b5a9
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-14 23:14:34 +0100

    Added bg image"

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spicy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spicy.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        // Get Action methood that retreive everything in the Category and pass it to the view
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+
+
+    }
+}
