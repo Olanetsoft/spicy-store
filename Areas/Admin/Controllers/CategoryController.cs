8ab93f896948694327679fdc279d42a73a38821b
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-15 01:05:38 +0100

    Added TableButtonPartial view

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spicy.Data;
+using Spicy.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,29 @@ namespace Spicy.Areas.Admin.Controllers
             return View(await _db.Category.ToListAsync());
         }
 
+
+        // GET page for Create Category
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                // If Valid
+                _db.Category.Add(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
 
     }
 }
