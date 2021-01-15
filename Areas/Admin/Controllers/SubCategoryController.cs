d55433937ba286a9fa2a3ac19e30f745c465d8f8
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-16 00:42:31 +0100

    Completed SubCategory View

---
+++ Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spicy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spicy.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class SubCategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // Get INDEX
+        public async Task<IActionResult> Index()
+        {
+            var subCategory = await _db.SubCategory.Include(s=>s.Category).ToListAsync();
+
+            return View(subCategory);
+        }
+    }
+}
