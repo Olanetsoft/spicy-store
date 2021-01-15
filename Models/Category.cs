a98c26ccdbb8970ff9fb177191393aebcc9da286
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-15 23:50:54 +0100

    Completed category DELETE View

--- Models/Category.cs
+++ Models/Category.cs
@@ -11,6 +11,7 @@ namespace Spicy.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name ="Category Name")]
         [Required]
         public string Name { get; set; }
     }
