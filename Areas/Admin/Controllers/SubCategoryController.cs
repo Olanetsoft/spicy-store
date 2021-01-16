65147f1146ee69bea76f5126616ac3c49fade4e4
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-16 12:10:16 +0100

    added extension Method for subcategory

--- Areas/Admin/Controllers/SubCategoryController.cs
+++ Areas/Admin/Controllers/SubCategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spicy.Data;
+using Spicy.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,21 @@ namespace Spicy.Areas.Admin.Controllers
             var subCategory = await _db.SubCategory.Include(s=>s.Category).ToListAsync();
 
             return View(subCategory);
+        }
+
+        // Get - CREATE
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+
+            };
+
+            return View(model)
+
         }
     }
 }
---
+++ Models/ViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spicy.Models.ViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+
+        public SubCategory SubCategory { get; set; }
+
+        public List<string> SubCategoryList { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
